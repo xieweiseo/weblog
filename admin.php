0b129f2f715b9ad588884cb02cfb8c26ffb10366
xieweiseo <xieweiseo@126.com>
2018-04-03 11:39:54 +0800

    ver.2018-0403-01

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 //开启调试模式
 define('APP_DEBUG',true);
 define('APP_NAME', 'Admin');
-//后台目录.
+//后台目录
 define('APP_PATH', './Admin/');
 define('W3CORE_PATH','./ThinkPHP');
 require W3CORE_PATH.'/ThinkPHP.php';
