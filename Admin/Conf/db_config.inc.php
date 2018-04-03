4e68c7a3be6caaf7e5fcaa3dec321d985dcfdb93
xieweiseo <xieweiseo@126.com>
2018-04-03 15:23:09 +0800

    ver.2018-0403-01

--- Admin/Conf/db_config.inc.php
+++ Admin/Conf/db_config.inc.php
@@ -14,7 +14,6 @@ return array(
 	'RBAC_NODE_TABLE'=>'wb_node',
 	'KEYCODE'=>'x53pc9',
     'APP_DEBUG'     =>  true,// 开启调试模式
-    //'DBYST_CONFIG' =>"mysql://hotel_query:hotel_query!^#@117.169.3.40:3306/hotel",
-     $DB_CONFIG => "mysql://dhtal:dhtal&%$#1U82@127.0.0.1:3306/panel"
+     $DB_CONFIG => "mysql://root:root@127.0.0.1:3306/panel"
 );
 ?>
