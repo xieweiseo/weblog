ac5b60833ac99025f963eafb0185c1e3c9b10bf3
hongyi <hongyi@126.com>
2018-05-08 10:56:00 +0800

    ...

--- Admin/Lib/Action/PanelAction.class.php
+++ Admin/Lib/Action/PanelAction.class.php
@@ -13,7 +13,7 @@ class PanelAction extends CommonAction{
 
     public function index() {
     	$hotel_id = I('hotel_id','','trim');
-    	$hotel_name = M('Hotel')->where('id='.$hotel_id.'and status=1')->getField('hotel_name');
+    	$hotel_name = M('Hotel')->where('id='.$hotel_id)->getField('hotel_name');
     	$lists = M('panel')->field('wb_panel.*,wb_hotel.hotel_name')->join('LEFT JOIN wb_hotel ON wb_hotel.id = wb_panel.hotel_id')->where('wb_panel.hotel_id='.$hotel_id.' and wb_panel.status=1')->order('wb_panel.sort ASC,wb_panel.id DESC')->select();
         //dump(M('panel')->getLastSql());
 	    $panel_list = array();
@@ -41,6 +41,7 @@ class PanelAction extends CommonAction{
 	public function insert($id = 0){
 	    $id = intval($id);
 	    $hotel_id = I('hotel_id');
+	    $hotel_name = M('Hotel')->where('id='.$hotel_id)->getField('hotel_name');
 	    if($id){
     	    $panel= M("Panel");
     	    $panel_info = $panel->getById($id);
@@ -68,6 +69,7 @@ class PanelAction extends CommonAction{
 		
 		//dump($panel_info);exit;		
         $this->assign('hotel_id',$hotel_id);
+        $this->assign('hotel_name',$hotel_name);
 	    $this->assign('panel_info',$panel_info);
 		$this->display();
 	}
@@ -115,8 +117,8 @@ class PanelAction extends CommonAction{
 	    }
 	    else{
 	        //新增
-	        $panel_name = $panel->where("panel_name='".$data['panel_name']."' and pid=0")->getField('panel_name');
-	        if(empty($panel_name)){
+	        //$panel_name = $panel->where("panel_name='".$data['panel_name']."' and pid=0")->getField('panel_name');
+	        //if(empty($panel_name)){
 		        $data['pid'] = $pid;
 		        $data['hotel_id'] = I('post.hotel_id','intval');
 		        //dump($data);exit;
@@ -127,10 +129,10 @@ class PanelAction extends CommonAction{
 		        else{
 		            $vo['message'] = '新增失败!';
 		        }
-	       }
-	       else{
-	       	   $vo['message'] = '已存在!';
-	       }
+	       //}
+	       //else{
+	       //	   $vo['message'] = '已存在!';
+	       //}
 	    }
 
 	   if ($list!==false) {
@@ -143,6 +145,7 @@ class PanelAction extends CommonAction{
 	//新增
 	public function create(){
 	    $hotel_id = I('hotel_id','','trim');
+	    $hotel_name = M('Hotel')->where('id='.$hotel_id)->getField('hotel_name');
 	    //获取panel列表
 	    $map = 'pid=0 and status=1 and hotel_id='.$hotel_id;
 	    $panel = M('Panel');
@@ -164,7 +167,7 @@ class PanelAction extends CommonAction{
 		//dump($node_info);exit;	
 		$hotel_id = I('hotel_id','intval');	
 		$this->assign('hotel_id',$hotel_id);
-	    //$this->assign('panel_info',$panel_info);
+	    $this->assign('hotel_name',$hotel_name);
 	    $this->assign('select',$select_tag);
 		$this->display();
 	}
@@ -188,11 +191,13 @@ class PanelAction extends CommonAction{
 	    $count = 0;
 	    foreach ($ids as $val){
 	       $count+= intval($panel->where(array('pid'=>$val))->count());
+	       //dump($panel->getLastSql());
 	    }
+	    //exit;
 	    //dump($node_info);exit;
 	    if($count>0){
 	        //dump($count);exit;
-	        $this->error('不能删除下面有子项!');
+	        $this->error('不能删除，下面有子项!');
 	    }
 	    else{
 	        $panel->where('id in ('.implode(',',$ids).')')->delete();
@@ -200,7 +205,194 @@ class PanelAction extends CommonAction{
 	        $this->success("删除成功");	        
 	    }
 	}
+	
+	//导入Panel文件
+	public function import(){
+	    $hotel_id = I('hotel_id');
+	    $hotel = M('Hotel');
+	    $hotel_info = $hotel->field('id,hotel_name')->where('id='.$hotel_id)->find();
 
+	    if (!empty ( $_FILES)){
+	        import("ORG.Net.UploadFile");
+	        $upload = new \UploadFile();
+	        $upload->maxSize   =  1048576000 ;
+	        $upload->allowExts =  array('xls','xlsx');
+	        $upload->savePath  = './Public/attached/excel/';
+	        $upload->uploadReplace = ture;//存在同名文件是否是覆盖
+	       
+	        // 上传文件
+	        $upload->upload();
+	        $info = $upload->getUploadFileInfo();
+	        $exts   = $info[0]['extension'];
+	        $filename = $upload->savePath.$info[0]['savename'];
+	        //dump($filename);exit;
+	        if(!$info) {
+	            //$upload->getError();
+	            $this->error('请上传导入文件！');
+	        }else{
+	            Vendor("PHPExcel/PHPExcel/");//引入phpexcel类(留意路径,不了解路径可以查看下手册)
+	            Vendor("PHPExcel/PHPExcel/IOFactory"); //引入phpexcel类(留意路径)
+	            if(strtolower($exts)=='xls')//判断excel表类型为2003还是2007
+	            {
+	                Vendor("PHPExcel/PHPExcel/Reader/Excel5"); //引入phpexcel类(留意路径)
+	                $objReader = PHPExcel_IOFactory::createReader('Excel5');
+	            }else if(strtolower($exts)=='xlsx'){
+	                Vendor("PHPExcel/PHPExcel/Reader/Excel2007");//引入phpexcel类(留意路径)
+	                $objReader = PHPExcel_IOFactory::createReader('Excel2007');
+	            }
+	            
+	            $PHPExcel = $objReader->load($filename);                     // 载入文件
+	            //dump($PHPExcel);exit;
+	            
+	            $currentSheet = $PHPExcel->getSheet(0);                      // 获取表中的第一个工作表，如果要获取第二个，把0改为1，依次类推
+	            $allColumn = $currentSheet->getHighestColumn();              // 获取总列数
+	            $allRow = $currentSheet->getHighestRow();                    // 获取总行数
+	            for ($currentRow = 2; $currentRow <= $allRow; $currentRow ++) {// 循环获取表中的数据，$currentRow表示当前行，从哪行开始读取数据，索引值从0开始
+	                for ($currentColumn = 'A'; $currentColumn <= $allColumn; $currentColumn ++) {// 从哪列开始，A表示第一列
+	                    $address = $currentColumn . $currentRow;             // 数据坐标
+	                    $ExlData[$currentRow][$currentColumn] = $currentSheet->getCell($address)->getValue();// 读取到的数据，保存到数组$arr中
+	              }
+	            }
+	            
+	            //dump($hotel_id);
+	            $panelData = array();
+	            foreach ($ExlData as $k=>$v){
+	                //dump($v['A']);
+	                //传hotel_id导单个酒店，不传则导所有酒店;	                
+	                if(!empty($v['A'])){
+	                    //$hotel_id = $hotel->where('hotel_name="'.$v['A'].'"')->getField('id');
+	                    //dump($hotel_id);
+	                    
+	                }    	                
+                    $panelData[$k] = $v;
+                    $panelData[$k]['A'] = $hotel_id;                
+	            }
+	                
+	            //dump($panelData); 
+	            //echo "<hr/>";
+	            //dump($ExlData);exit;
+	            $result = $this->importExcel_RAW($panelData,$hotel_id);
+	            if ($result['error']==0) {
+	                $this->redirect('panel/index',array('hotel_id'=>$result['hotel_id']));
+	            }
+	            else {
+	                $this->error("导入失败！");// 提示错误
+	            }
+	        }
+	    }
+	    else{
+	        //dump($hotel_info);
+	        $this->assign('hotel_info',$hotel_info);
+	        $this->display();
+	    }
+	}
+	
+	//导入Panel数据
+	public function  importExcel_RAW($ExlData, $hotel_id = 0){   // 将导入表中的数据添加到  数据库数组中去
+	    $hotel = M('Hotel');
+	    $panel = M('Panel');
+	    $create_time = date('Y-m-d H:i:s');
+	    $import_number = 0;
+	    //验证不能为空
+	    for($i = 2,$j=0;$i<=sizeof($ExlData)+1;$i++,$j++){
+	        if(empty($ExlData[$i]['A'])){
+	            $this->error("第:".$i++."行不能为空!");
+	        }
+	        if(empty($ExlData[$i]['B'])){
+	            $this->error("第:".$i++."行不能为空!");
+	        }
+	        if(empty($ExlData[$i]['C'])){
+	            $this->error("第:".$i++."行不能为空!");
+	        }
+	        if(empty($ExlData[$i]['D'])){
+	            $this->error("第:".$i++."行不能为空!");
+	        }
+	        if(empty($ExlData[$i]['E'])){
+	            $this->error("第:".$i++."行不能为空!");
+	        }
+	        
+	    }
+	    //新增修改面板
+	    $panelData = array();
+	    foreach ($ExlData as $k=>$v){
+	          if(in_array($v['B'], $panelData)==false){
+	               $panelData[$v['D']] = $v['B'];
+	               $id = $panel->where('panel_name="'.$v['B'].'" and hotel_id='.$hotel_id)->getField('id');
+	               $data['hotel_id'] = $hotel_id;
+	               $data['panel_no'] = $v['D'];
+	               $data['panel_name'] = $v['B'];
+	               if($id){
+	                   //更新
+	                   //dump($data);
+	                   //$panel->data($data)->where('id='.$id)->save();
+	               }
+	               else{
+	                   //新增	                   
+	                   $panel->data($data)->add();
+	               }
+	               
+	          }
+	    }
+	    //dump($ExlData);exit;
+	    //新增修改推荐位
+	    $result = array('error'=>0,'messge'=>'','hotel_id'=>$hotel_id);
+	    for($i = 2,$j=0;$i<=sizeof($ExlData)+1;$i++,$j++){
+	        $dataList[] = array(
+	            'hotel_id'=>trim($ExlData[$i]['A']),
+	            'panel_name'=> trim($ExlData[$i]['B']),
+	            'block_name'=>trim($ExlData[$i]['C']),
+	            'panel_no'=>strtolower(trim($ExlData[$i]['D'])),
+	            'block_no'=>strtolower(trim($ExlData[$i]['E'])),
+	            'status'=>1,
+	            'sort' =>100,
+	            'remark'=>'',
+	        );
+	        try{
+	            //不存在则添加	            
+	            $panel_id = $panel->where('hotel_id='.trim($ExlData[$i]['A']).' and panel_name="'.trim($ExlData[$i]['B']).'"')->getField('id');
+	            $block_id = $panel->where('hotel_id='.trim($ExlData[$i]['A']).' and panel_name="'.trim($ExlData[$i]['C']).'" and pid='.$panel_id)->getField('id');
+	            
+	            $dataList[$j]['pid'] = $panel_id;
+	            $dataList[$j]['block_no'] = $ExlData[$i]['E'];
+	            $dataList[$j]['panel_name'] = $ExlData[$i]['C'];
+	            
+	            if(!$block_id){
+	                //新增推荐位
+	                $panel_id_pn = $panel->data($dataList[$j])->add();	               
+	            }
+	            else{
+	                //更新推荐位	                
+	                //$panel->data($dataList[$j])->where('hotel_id='.$dataList[$j]['hotel_id'].' and id='.$block_id)->save();
+	            }
+	           
+	        }catch (\Exception $e){
+	            //echo $e->getMessage();
+	            $result['error'] = 1;
+	            $result['message'] = $e->getMessage();
+	        }
+	        
+	    }
+	    //dump($result);exit;
+	    return $result;
+	}   
+	
+	public function panel_template(){
+	    header("Content-type:text/html;charset=utf-8");
+	    $save_name = 'panel_template.xlsx';
+	    $file_path = $_SERVER['DOCUMENT_ROOT'].'/data/excel_template/'.$save_name;
+	    ob_end_clean();
+	    $hfile = fopen($file_path, "rb") or die("Can not find file: $file_path\n");
+	    Header("Content-type: application/octet-stream");
+	    Header("Content-Transfer-Encoding: binary");
+	    Header("Accept-Ranges: bytes");
+	    Header("Content-Length: ".filesize($file_path));
+	    Header("Content-Disposition: attachment; filename=\"$save_name\"");
+	    while (!feof($hfile)) {
+	        echo fread($hfile, 32768);
+	    }
+	    fclose($hfile);
+	    exit;
+	} 
 
 }
 ?>
